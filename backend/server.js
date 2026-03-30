6b1b3e0515d6f474cdb4b6e6091318ea65a2555a
Badhant Chettri <chettribadhant@gmail.com>
2026-03-30 15:54:44 +0530

    fix cors issue

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.options('*',cors());
 
 // MongoDB Connection (Atlas)
 mongoose.connect(process.env.MONGO_URI)
