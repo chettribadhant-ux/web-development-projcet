1e159b1ab339b41dfcad58a4c9a61900d23024be
Badhant Chettri <chettribadhant@gmail.com>
2026-03-30 11:16:06 +0530

    initial commit:backend and frontend

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+const dns =require('dns');
+dns.setServers(['8.8.8.8','8.8.4.4']);
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const Contact = require("./models/Contact");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection (Atlas)
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Atlas Connected"))
+.catch(err => console.log("DB Error:", err));
+
+// API route
+app.post("/api/contact", async (req, res) => {
+  try {
+    const { name, email, message } = req.body;
+
+    const newContact = new Contact({ name, email, message });
+    await newContact.save();
+
+    res.status(201).json({ success: true, message: "Message saved" });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+// Start server (IMPORTANT for Render)
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
